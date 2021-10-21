06ba7ac56f25baeb6b90d40b0544c5a623e85d9f
spencercreer <spencercreer@gmail.com>
2021-10-21 15:23:56 -0700

    setup models

---
+++ server/models/Book.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose')
+
+const BookSchema = new Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    pages: {
+        type: Number,
+        required
+    }
+})
+
+const Book = model('Book', BookSchema)
+
+module.exports = Book
