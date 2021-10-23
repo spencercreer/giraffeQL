8ab98aeb44a6f179c36ae823645eaaa37ea40307
spencercreer <spencercreer@gmail.com>
2021-10-23 05:16:55 -0700

    added BookList and Home components

---
+++ client/src/components/BookList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Card, CardText, CardHeader, CardTitle, CardBody } from 'reactstrap'
+
+const BookList = ({books}) => {
+    if(!books.length) {
+        return <h3>Thare no books currently in the library...</h3>
+    }
+
+    return (
+        <div>
+            {book
+                && books.map((book) => (
+                    <Card>
+                        <CardHeader>Title: {book.title}</CardHeader>
+                        <CardBody>
+                            <CardTitle>Author: {book.author}</CardTitle>
+                            <CardText>Number of pages: {book.pages}</CardText>
+                        </CardBody>
+                    </Card>
+                ))}
+        </div>
+    )
+}
